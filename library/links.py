0247b0ff6a15db24e9c9e35a9301415b693c3192
Qxe5 <NULL>
2022-01-15 08:22:34 +0000

    add scam links

---
+++ library/links.py
@@ -0,0 +1,14 @@
+'''Transient management of scam links'''
+import requests
+
+links = []
+
+async def update():
+    '''Updates scam links'''
+    source = 'https://raw.githubusercontent.com/DevSpen/scam-links/master/src/links.txt'
+    response = requests.get(source)
+
+    if not response.ok:
+        return
+
+    links.extend(response.text.splitlines())
