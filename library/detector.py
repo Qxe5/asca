a9d78886607e588a2703aae271083c6bf9eac7e8
Qxe5 <cypherspace@icloud.com>
2022-03-29 04:36:43 +0100

    add mappings to decyrillic()

--- library/detector.py
+++ library/detector.py
@@ -38,10 +38,19 @@ async def official(link):
 async def decyrillic(text):
     '''Transform Cyrillic into ASCII and return the transformation'''
     replacements = {
+        ('з', '3'),
+        ('ч', '4'),
         ('а', 'a'),
+        ('в', 'b'),
         ('с', 'c'),
         ('е', 'e'),
+        ('н', 'h'),
+        ('к', 'k'),
+        ('м', 'm'),
         ('о', 'o'),
+        ('р', 'p'),
+        ('т', 't'),
+        ('х', 'x'),
         ('у', 'y')
     }
 
