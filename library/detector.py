e383481d7532da112d3033fc24bee1fcf0d476f3
Qxe5 <cypherspace@icloud.com>
2022-10-04 06:42:51 +0100

    increase message delete delay

--- library/detector.py
+++ library/detector.py
@@ -243,7 +243,7 @@ async def prune(messages):
     async with deletelock:
         for message in messages:
             await delete(message)
-            await sleep(4)
+            await sleep(5)
 
 async def punish(message):
     '''Punish the member which sent the message and return whether the punishment was succesfull'''
